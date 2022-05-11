2fdca3259f37640910ffb340986c33167299ea58
OONISHI REON <oonishireon110@gmail.com>
2022-05-11 14:07:26 +0900

    強制アンラップを利用

--- ios-trainingTests/ios_trainingTests.swift
+++ ios-trainingTests/ios_trainingTests.swift
@@ -11,54 +11,39 @@ import XCTest
 final class ios_trainingTests: XCTestCase {
     
     private var weatherDisplayViewController: WeatherDisplayViewController!
-    private enum TestError: Error {
-        case failedGetWeatherImage
-        case failedGetMaxWeatherText
-        case failedGetMinWeatherText
-    }
     
     func test天気予報がsunnyだったら画面に晴れ画像が表示されること() throws {
         let sunnyWeather = Weather(maxTemp: 10, minTemp: 0, weather: "sunny")
         setupWeatherDisplay(weather: sunnyWeather)
-        guard let image = weatherDisplayViewController.weatherImageView?.image else {
-            throw TestError.failedGetWeatherImage
-        }
+        let image = weatherDisplayViewController.weatherImageView!.image
         XCTAssertEqual(image, UIImage(named: "sunny"))
     }
     
     func test天気予報がcloudyだったら画面に曇り画像が表示されること() throws {
         let cloudyWeather = Weather(maxTemp: 10, minTemp: 0, weather: "cloudy")
         setupWeatherDisplay(weather: cloudyWeather)
-        guard let image = weatherDisplayViewController.weatherImageView?.image else {
-            throw TestError.failedGetWeatherImage
-        }
+        let image = weatherDisplayViewController.weatherImageView!.image
         XCTAssertEqual(image, UIImage(named: "cloudy"))
     }
     
     func test天気予報がrainyだったら画面に雨画像が表示されること() throws {
         let rainyWeather = Weather(maxTemp: 10, minTemp: 0, weather: "rainy")
         setupWeatherDisplay(weather: rainyWeather)
-        guard let image = weatherDisplayViewController.weatherImageView?.image else {
-            throw TestError.failedGetWeatherImage
-        }
+        let image = weatherDisplayViewController.weatherImageView!.image
         XCTAssertEqual(image, UIImage(named: "rainy"))
     }
     
     func test天気予報の最高気温がUILabelに反映されること() throws {
         let weather = Weather(maxTemp: 10, minTemp: 0, weather: "sunny")
         setupWeatherDisplay(weather: weather)
-        guard let maxTempText = weatherDisplayViewController.maxTemperatureLabel.text else {
-            throw TestError.failedGetMaxWeatherText
-        }
+        let maxTempText = weatherDisplayViewController.maxTemperatureLabel.text
         XCTAssertEqual(maxTempText, "10")
     }
     
     func test天気予報の最低気温がUILabelに反映されること() throws {
         let weather = Weather(maxTemp: 10, minTemp: 0, weather: "sunny")
         setupWeatherDisplay(weather: weather)
-        guard let minTempText = weatherDisplayViewController.minTemperatureLabel.text else {
-            throw TestError.failedGetMinWeatherText
-        }
+        let minTempText = weatherDisplayViewController.minTemperatureLabel.text
         XCTAssertEqual(minTempText, "0")
     }
     
